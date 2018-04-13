abc6f09784a820316be567633b7d1d1c37693d64
Varun <varunlakshmananap@gmail.com>
2018-04-13 08:52:17 +0530

    Expose TelegramShare Icon fom package

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export { default as WhatsAppShare }  from './share/whatsapp';
 export { default as RedditShare }  from './share/reddit';
 export { default as LinkedinShare }  from './share/linkedin';
 export { default as PinterestShare }  from './share/pinterest';
+export { default as TelegramShare }  from './share/telegram';
 export { default as EmailShare }  from './share/email';
 
 export { default as FacebookIcon }  from './icons/facebook';
@@ -12,4 +13,5 @@ export { default as WhatsAppIcon }  from './icons/whatsapp';
 export { default as RedditIcon }  from './icons/reddit';
 export { default as LinkedinIcon }  from './icons/linkedin';
 export { default as PinterestIcon }  from './icons/pinterest';
+export { default as TelegramIcon }  from './icons/telegram';
 export { default as EmailIcon }  from './icons/email';
